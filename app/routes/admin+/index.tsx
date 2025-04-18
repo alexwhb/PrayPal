d32d786f9ef3687bfb08d2bc520064b6cbe7f201
Alex Black <alexwhb@gmail.com>
2025-04-17 17:26:22 -0700

    Adds placeholder admin view.

---
+++ app/routes/admin+/index.tsx
@@ -0,0 +1,15 @@
+// TODO. This will be really mostly just a panel where you can see the current
+// stats of the app, such as how many users are registered, how many posts are
+// created, etc. Oh there should also be a view where the admin can modify the
+// available categories for prayers and needs.
+
+// There should also be a site settings page where the admin can modify
+// things like, the sites name... they should also be able to change things like
+// are users automatically allowed to post once an account has been created? Or
+// do they need to be approved by an admin first? Since this is potentially
+// just for a single small church body, that should be quite possible, and
+// would massively reduce the amount of spam that gets posted.
+
+export default function Index() {
+	return <main>Admin</main>
+}
