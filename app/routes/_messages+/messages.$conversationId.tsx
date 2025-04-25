fd03208a902a2d4505e4d864b61c492ec21389c9
Alex Black <alexwhb@gmail.com>
2025-04-24 17:47:35 -0700

    Updates our websocket to use the same port as the app.

--- app/routes/_messages+/messages.$conversationId.tsx
+++ app/routes/_messages+/messages.$conversationId.tsx
@@ -122,7 +122,7 @@ export async function action({ request }: Route.ActionArgs) {
 	const socket = io(
 		process.env.NODE_ENV === "production" 
 			? "https://your-app.fly.dev" 
-			: "http://localhost:3001",
+			: "http://localhost:3000",
 		{ withCredentials: true }
 	)
 	
